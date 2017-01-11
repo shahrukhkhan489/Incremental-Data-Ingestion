29e895f364429e3f40f9c2f139b1bd7eddf64f13
Shahrukh Khan <shahrukhkhan489@gmail.com>
2017-01-12 02:43:19 +0530

    Single and Dual Flow

--- Scripts/4-display_data_from_particular_date.sh
+++ Scripts/4-display_data_from_particular_date.sh
@@ -3,5 +3,5 @@
 cat > ~/selecttablequery.hql <<EOF
 SELECT * FROM base_table where dt='$1';
 EOF
-hive -f ~/selecttablequery.hql;
+hive -f ~/selecttablequery.hql > ~/$1-queryoutput.log;
 rm -rf ~/selecttablequery.hql;
